ea8c0f0b115ebcd8bd99823e96bbad8e6ca01c6c
Kelp Green <gerard.labelle+github@gmail.com>
2013-04-22 21:06:19 -0700

    Made static pages Spec file more compact.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,65 +2,43 @@ require 'spec_helper'
 
 describe "StaticPages" do
   let(:base_title) { 'Ruby on Rails Tutorial Primero App' }
-  
-  describe 'About Us page' do
-    
-    it "should have the h1 'About Us'" do
-      visit about_path
-      page.should have_selector('h1', :text => 'About Us')
-    end
+  subject { page }
 
-    it "should have the right title" do
-      visit about_path
-      page.should have_selector('title',
-        :text => "#{base_title} | About Us")
-    end
+  describe 'About Us page' do
+    before { visit about_path }
+
+    it { should have_selector 'h1', text: 'About Us' }
+    
+    it { should have_selector 'title', text: full_title('About Us')  }
     
   end
 
   describe 'Contact Us page' do
-    
-    it "should have the h1 'Contact Us'" do
-      visit contact_path
-      page.should have_selector('h1', :text => 'Contact Us')
-    end
-    
-    it "should have the right title" do
-      visit contact_path
-      page.should have_selector('title', :text => "#{base_title} | Contact Us")
-    end
+    before { visit contact_path }
+
+    it { should have_selector 'h1', text: 'Contact Us' }
+
+    it { should have_selector 'title', text: full_title('Contact Us') }
+
   end
   
   describe 'Help page' do
-    
-    it "should have the h1 'Help'" do
-      visit help_path
-      page.should have_selector('h1', :text => 'Help')
-    end
+    before { visit help_path }
 
-    it "should have the right title" do
-      visit help_path
-      page.should have_selector('title', :text => "#{base_title} | Help")
-    end
+    it { should have_selector 'h1', text: 'Help' }
+    
+    it { should have_selector 'title', text: full_title('Help') }
     
   end
   
   describe "Home page" do
-    
-    it "should have the h1 'Primero App'" do
-      visit root_path
-      page.should have_selector('h1', text: 'Primero App')
-    end
+    before { visit root_path }
 
-    it "should have the base title" do
-      visit root_path
-      page.should have_selector('title', text: "#{base_title}")
-    end
+    it { should have_selector 'h1', text: 'Primero App' }
+
+    it { should have_selector 'title', text: full_title('')}
     
-    it "should not have a cutome page title" do
-      visit root_path
-      page.should_not have_selector('title', text: '| Home')
-    end
+    it { should_not have_selector 'title', text: '| Home' }
   end
   
 end
